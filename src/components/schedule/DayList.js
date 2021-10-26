a64e785f5608ab0c14e4cb008c4ac0228f8e6ece
Matt F <trumpetman072392@gmail.com>
2021-10-26 10:16:24 -0500

    added form to add medication to everyday

--- src/components/schedule/DayList.js
+++ src/components/schedule/DayList.js
@@ -1,12 +1,16 @@
 import React from "react";
 import { useEffect, useState } from "react/cjs/react.development";
 import { getAllDays } from "../../modules/DayManager";
+import { Button } from "reactstrap";
 import { DailyCard } from "./DailyCard";
+import { useHistory } from "react-router";
 
 
 
 export const DayList = () => {
     const [days, setDays] = useState([])
+
+    const history = useHistory()
 
     const getDays = () => {
         return getAllDays().then(response => {
@@ -20,7 +24,15 @@ export const DayList = () => {
 
     return (
         <>
-            <h1 className="listTitles"> Weekly Medication Schedule</h1>
+            <div className="addAllContainer">
+                <h1 className="listTitles"> Weekly Medication Schedule</h1>
+                <Button type="button"
+                    className="addAllButton"
+                    variant="secondary" size="sm"
+                    onClick={() => history.push("/create")}>
+                    Add Medication to Everyday
+                </Button>
+            </div>
             <hr></hr>
             <section className="dailyList">
                 {days.map(day => <DailyCard day={day} key={day.id} />)}
